6e45a44ccc9a8fa5041b8b789671eaab9988db03
Garrett Hays <GarrettlHays@gmail.com>
2022-06-28 10:45:33 -0700

    separate business logic into separate file
    Co-authored-by: Kyle Chinick <kylechinick@icloud.com>
    Co-authored-by: Alex McKnight <alex.mcknight11@gmail.com>

---
+++ src/TriviaGame.js
@@ -0,0 +1,17 @@
+export default class TriviaGame {  
+  static getTrivia(numberOfQuestions, category, difficulty) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=multiple`;
+        request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    })
+  }
+}
